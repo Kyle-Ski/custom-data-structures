3e98e3b1fe0dfb6e9fc32494ced3d95fb44bbbbf
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-12-11 11:42:29 -0700

    got it

--- classes.js
+++ classes.js
@@ -1,8 +1,32 @@
 // Create custom data structures
 // 1. Code-along here
+class Spell {
+    constructor(name, forgive, type, diff) {
+        this.name = name
+        this.forgive = forgive
+        this.type = type
+        this.difficulty = diff
+    }
+    setDiffuculty(value){
+        this.difficulty = value
+    }
+}
+// an instance of the class
+let patronus = new Spell({name: 'Partonus', forgive: true, type: 'defense', diff: 7})
+// console.log(patronus)
+patronus.setDiffuculty(10)
+// console.log('new',patronus)
 
-
-
+class AttackSpell extends Spell {
+    constructor(dam, ...args) {
+        super(...args)
+        this.type = 'Attack'
+        this.dammage = dam
+    }
+}
+let expelliamus = new AttackSpell(7.9, 'Expelliamus', false, 'k', 4)
+expelliamus.setDiffuculty(10)
+console.log(expelliamus)
 // 2. Create your own class
 
 
